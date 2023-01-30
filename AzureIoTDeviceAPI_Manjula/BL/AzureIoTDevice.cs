978247a2bef61c11f30b5165006f168ac333526a
manjulakadur <122857555+manjulakadur@users.noreply.github.com>
2023-01-30 11:58:02 +0530

    Update AzureIoTDevice.cs
    Connection string updated

--- AzureIoTDeviceAPI_Manjula/BL/AzureIoTDevice.cs
+++ AzureIoTDeviceAPI_Manjula/BL/AzureIoTDevice.cs
@@ -16,7 +16,7 @@ namespace AzureIoTDeviceAPI_Manjula.BL
        
         private DeviceClient s_deviceClient;
         DeviceClient Client = null;
-        static string DeviceConnectionString = "HostName=iothub270123.azure-devices.net;SharedAccessKeyName=iotreg;SharedAccessKey=7niuWRLwDNazYAOsDBKe4TZsRcJpOcOOHza+klaH/WY=";
+        static string DeviceConnectionString = "HostName=iothub300123.azure-devices.net;SharedAccessKeyName=iothubsharedaccess;SharedAccessKey=y2LBThmX19xHgGU4KdTvCEyUT2Jws126teHqMcv9Tys=";
         RegistryManager registryManager = RegistryManager.CreateFromConnectionString(DeviceConnectionString);
         public async Task<string> AddDeviceAsync(string deviceId)
         {
