c4a0cca8714718153bc6a68fff6659f889f523ca
manjulakadur <122857555+manjulakadur@users.noreply.github.com>
2023-02-02 11:25:27 +0530

    Update AzureIoTDevice.cs
    ok

--- AzureIoTDeviceAPI_Manjula/BL/AzureIoTDevice.cs
+++ AzureIoTDeviceAPI_Manjula/BL/AzureIoTDevice.cs
@@ -1,4 +1,4 @@
-﻿using AzureIoTDeviceAPI_Manjula.Service;
+using AzureIoTDeviceAPI_Manjula.Service;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Common;
@@ -13,42 +13,45 @@ namespace AzureIoTDeviceAPI_Manjula.BL
 {
     public class AzureIoTDevice : IAzureIoTDevice
     {
-       
+
         private DeviceClient s_deviceClient;
         DeviceClient Client = null;
         static string DeviceConnectionString = "HostName=iothubtestdemo1.azure-devices.net;SharedAccessKeyName=iothubtestdemo1;SharedAccessKey=J9+yF1/v+WRMxpgu3S7UoZy5oz6bdb/Irw4yCLGEHTQ=";
         RegistryManager registryManager = RegistryManager.CreateFromConnectionString(DeviceConnectionString);
         public async Task<string> AddDeviceAsync(string deviceId)
         {
+            string connectionstring = string.Empty;
             Device device;
             try
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    string newDeviceId = deviceId + i;
 
-                    device = await registryManager.AddDeviceAsync(new Device(newDeviceId));
-                   // Console.WriteLine("Created device :{0}", newDeviceId);
+                string newDeviceId = deviceId;
 
-                }
-                return "Devices Created";
+                device = await registryManager.AddDeviceAsync(new Device(newDeviceId));
+
+                connectionstring = string.Format("HostName={0};DeviceId={1};SharedAccessKey={2}",
+                "iothubtestdemo1.azure-devices.net", device.Id, device.Authentication.SymmetricKey.PrimaryKey);
+
+                return "Device Created :- " + connectionstring;
             }
             catch (DeviceAlreadyExistsException)
-            {                
+            {
                 device = await registryManager.GetDeviceAsync(deviceId);
                 return "Already existing device:";
             }
-           //return $"Generated device key:{device.Authentication.SymmetricKey.PrimaryKey}";
+            //return $"Generated device key:{device.Authentication.SymmetricKey.PrimaryKey}";
         }
         public async Task<List<string>> GetAllDevices()
         {
+            string connectionstring = string.Empty;
             try
             {
                 var devices = await registryManager.GetDevicesAsync(100);
                 List<string> allDevices = new List<string>();
                 foreach (var device in devices)
                 {
-                    allDevices.Add($"Id {device.Id} - Status {device.Status} - Reason {device.StatusReason}");
+                    connectionstring = string.Format("HostName={0};DeviceId={1};SharedAccessKey={2}", "iothubtestdemo1.azure-devices.net", device.Id, device.Authentication.SymmetricKey.PrimaryKey);
+                    allDevices.Add($"Id {device.Id} - Status {device.Status} - Reason {device.StatusReason} - Connection string - {connectionstring}");
                 }
                 return allDevices;
             }
@@ -113,16 +116,16 @@ namespace AzureIoTDeviceAPI_Manjula.BL
         {
             try
             {
-                
-               /* string connectionString = string.Format("HostName=manjulaIoTTest.azure-devices.net;DeviceId={0};SharedAccessKey=GWmijvSchtl8eoezabXR3cBa46cbaL9Nb7nwazGwKW0=",
-                              DeviceName);*/
+
+                /* string connectionString = string.Format("HostName=manjulaIoTTest.azure-devices.net;DeviceId={0};SharedAccessKey=GWmijvSchtl8eoezabXR3cBa46cbaL9Nb7nwazGwKW0=",
+                               DeviceName);*/
 
                 DeviceClient s_deviceClient = DeviceClient.CreateFromConnectionString(connectionString, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
                 double minTemperature = 20;
                 double minHumidity = 60;
                 Random rand = new Random();
-                
-               // while (true)
+
+                // while (true)
                 {
                     double currentTemperature = minTemperature + rand.NextDouble() * 15;
                     double currentHumidity = minHumidity + rand.NextDouble() * 20;
@@ -149,7 +152,7 @@ namespace AzureIoTDeviceAPI_Manjula.BL
                     // Send the telemetry message  
                     await s_deviceClient.SendEventAsync(message);
                     return DateTime.Now + " > Sending message: " + messageString;
-                   // await Task.Delay(1000 * 10);
+                    // await Task.Delay(1000 * 10);
 
                 }
             }
@@ -160,72 +163,89 @@ namespace AzureIoTDeviceAPI_Manjula.BL
             }
         }
 
-        public async Task<string> DesiredPropertiesUpdate(string DeviceName)
-        {            
+        public async Task<string> DesiredPropertiesUpdate(string DeviceName, string desiredPropertyValue)
+        {
+
+            var registryManager = RegistryManager.CreateFromConnectionString(DeviceConnectionString);
             var twin = await registryManager.GetTwinAsync(DeviceName);
-            var patch =
-                @"{
-            tags: {
-                location: {
-                    region: 'US',
-                    plant: 'Redmond43'
-                }
-            }
-        }";
-            await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+            twin.Properties.Desired["devicedesiredproperty"] = desiredPropertyValue;
+            await registryManager.UpdateTwinAsync(twin.DeviceId, twin, twin.ETag);
+            return "Desired Property Updated";
 
-            var query = registryManager.CreateQuery(
-              "SELECT * FROM devices WHERE tags.location.plant = 'Redmond43'", 100);
-            var twinsInRedmond43 = await query.GetNextAsTwinAsync();
-            string returnmsg1 = string.Empty;
-            returnmsg1 = "Devices in Redmond43: "+
-              string.Join(", ", twinsInRedmond43.Select(t => t.DeviceId));
 
-            query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43' AND properties.reported.connectivity.type = 'cellular'", 100);
-            var twinsInRedmond43UsingCellular = await query.GetNextAsTwinAsync();
-            string returnmsg2 = string.Empty;
-            returnmsg2 = "Devices in Redmond43 using cellular network: "+
-              string.Join(", ", twinsInRedmond43UsingCellular.Select(t => t.DeviceId));
-            return returnmsg1+ System.Environment.NewLine + returnmsg2 + System.Environment.NewLine +
-                "Desired Properties updated";
+            //    var twin = await registryManager.GetTwinAsync(DeviceName);
+            //    var patch =
+            //        @"{
+            //    tags: {
+            //        location: {
+            //            region: 'US',
+            //            plant: 'Redmond43'
+            //        }
+            //    }
+            //}";
+            //    await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+
+            //    var query = registryManager.CreateQuery(
+            //      "SELECT * FROM devices WHERE tags.location.plant = 'Redmond43'", 100);
+            //    var twinsInRedmond43 = await query.GetNextAsTwinAsync();
+            //    string returnmsg1 = string.Empty;
+            //    returnmsg1 = "Devices in Redmond43: "+
+            //      string.Join(", ", twinsInRedmond43.Select(t => t.DeviceId));
+
+            //    query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43' AND properties.reported.connectivity.type = 'cellular'", 100);
+            //    var twinsInRedmond43UsingCellular = await query.GetNextAsTwinAsync();
+            //    string returnmsg2 = string.Empty;
+            //    returnmsg2 = "Devices in Redmond43 using cellular network: "+
+            //      string.Join(", ", twinsInRedmond43UsingCellular.Select(t => t.DeviceId));
+            //    return returnmsg1+ System.Environment.NewLine + returnmsg2 + System.Environment.NewLine +
+            //        "Desired Properties updated";
         }
 
         //reported properties
-       /* public async Task<string> InitClient(string DeviceName)
+        /* public async Task<string> InitClient(string DeviceName)
+         {
+             try
+             {                
+                 Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString, DeviceName,
+                   Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+                 //Console.WriteLine("Retrieving twin");
+                 await Client.GetTwinAsync();
+                 return "Retrieving twin";
+             }
+             catch (Exception ex)
+             {
+                return "Error in sample:" + ex.Message;
+             }
+         }*/
+        public async Task<string> ReportConnectivity(string connectionString, string DeviceName, string reportPropertyValue)
         {
             try
-            {                
-                Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString, DeviceName,
-                  Microsoft.Azure.Devices.Client.TransportType.Mqtt);
-                //Console.WriteLine("Retrieving twin");
-                await Client.GetTwinAsync();
-                return "Retrieving twin";
-            }
-            catch (Exception ex)
             {
-               return "Error in sample:" + ex.Message;
-            }
-        }*/
-        public async Task<string> ReportConnectivity(string DeviceName)
-        {
-            try
-            {
-                //Console.WriteLine("Sending connectivity data as reported property");
-                Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString, DeviceName,
-                 Microsoft.Azure.Devices.Client.TransportType.Mqtt);
-                //Console.WriteLine("Retrieving twin");
-                await Client.GetTwinAsync();
-                TwinCollection reportedProperties, connectivity;
-                reportedProperties = new TwinCollection();
-                connectivity = new TwinCollection();
-                connectivity["type"] = "cellular";
-                reportedProperties["connectivity"] = connectivity;
+                ////Console.WriteLine("Sending connectivity data as reported property");
+                //Client = DeviceClient.CreateFromConnectionString(connectionString, DeviceName,
+                // Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+                ////Console.WriteLine("Retrieving twin");
+                //await Client.GetTwinAsync();
+                //TwinCollection reportedProperties, connectivity;
+                //reportedProperties = new TwinCollection();
+                //connectivity = new TwinCollection();
+                //connectivity["type"] = "cellular";
+                //reportedProperties["connectivity"] = connectivity;
+                //await Client.UpdateReportedPropertiesAsync(reportedProperties);
+                //return "Reported Properties Updated";
+
+                var Client = DeviceClient.CreateFromConnectionString(connectionString, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+
+                Microsoft.Azure.Devices.Shared.TwinCollection reportedProperties;
+                reportedProperties = new Microsoft.Azure.Devices.Shared.TwinCollection();
+                reportedProperties["connectivity"] = reportPropertyValue;
                 await Client.UpdateReportedPropertiesAsync(reportedProperties);
-                return "Reported Properties Updated";
+
+                return "Report Property Updated";
             }
             catch (Exception ex)
-            {                
-               return "Error in sample: " + ex.Message;
+            {
+                return "Error in sample: " + ex.Message;
             }
         }
     }
